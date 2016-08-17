b11ba837bf0fc31ddd99eef18b43e18f7d947aa4
Niek Tax <niektax@gmail.com>
2016-08-17 11:11:13 +0200

    memorycleanup operator now shuts down threads of child plugincontexts

--- src/main/java/org/rapidprom/operators/util/RapidProMMemoryCleanUpOperator.java
+++ src/main/java/org/rapidprom/operators/util/RapidProMMemoryCleanUpOperator.java
@@ -1,6 +1,11 @@
 package org.rapidprom.operators.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.processmining.framework.plugin.PluginContext;
 import org.rapidprom.external.connectors.prom.RapidProMGlobalContext;
+import org.rapidprom.external.connectors.prom.RapidProMPluginContext;
 
 import com.rapidminer.operator.MemoryCleanUp;
 import com.rapidminer.operator.OperatorDescription;
@@ -14,7 +19,15 @@ public class RapidProMMemoryCleanUpOperator extends MemoryCleanUp {
 
 	@Override
 	public void doWork() throws OperatorException {
-		RapidProMGlobalContext.instance().getPluginContext().clear();
+		PluginContext context = RapidProMGlobalContext.instance().getPluginContext();
+		List<PluginContext> children = new ArrayList<PluginContext>(context.getChildContexts());
+		for(PluginContext childContext : children){
+			if(childContext instanceof RapidProMPluginContext)
+				((RapidProMPluginContext) childContext).closeExecutor();
+			context.deleteChild(childContext);
+		}
+		context.clear();
+		
 		super.doWork();
 	}
 
