a658ecc5c612269eed6f54c158c14c19013ee616
Niek Tax <niektax@gmail.com>
2016-08-17 11:12:56 +0200

    operators for niek's experiments

---
+++ src/main/java/org/rapidprom/operators/niek/OnlyContainCompleteEventsOperator.java
@@ -0,0 +1,68 @@
+package org.rapidprom.operators.niek;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
+import org.deckfour.xes.model.impl.XTraceImpl;
+import org.processmining.framework.plugin.PluginContext;
+import org.rapidprom.external.connectors.prom.RapidProMGlobalContext;
+import org.rapidprom.ioobjects.XLogIOObject;
+
+import com.rapidminer.operator.IOObjectCollection;
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.ports.InputPort;
+import com.rapidminer.operator.ports.OutputPort;
+import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
+import com.rapidminer.tools.LogService;
+
+public class OnlyContainCompleteEventsOperator extends Operator {
+
+	private InputPort inputXLog = getInputPorts()
+			.createPort("event log (ProM Event Log)", XLogIOObject.class);
+	private OutputPort outputEventLog = getOutputPorts()
+			.createPort("event log (ProM Event Log)");
+
+	public OnlyContainCompleteEventsOperator(OperatorDescription description) {
+		super(description);
+		getTransformer().addRule(new GenerateNewMDRule(outputEventLog,
+				XLogIOObject.class));
+	}
+
+	public void doWork() throws OperatorException {
+		Logger logger = LogService.getRoot();
+		logger.log(Level.INFO, "Start: Only contain lifecycle complete events");
+		long time = System.currentTimeMillis();
+
+		XLogIOObject logWrapper = inputXLog.getData(XLogIOObject.class);
+
+		XLog log = logWrapper.getArtifact();
+		XLog clone = (XLog) log.clone();
+		for(XTrace trace : clone){
+			List<XEvent> newTrace = new LinkedList<XEvent>();
+			for(XEvent event : trace){
+				if(event.getAttributes().get("lifecycle:transition").toString().equals("complete"))
+					newTrace.add(event);
+			}
+			trace = new XTraceImpl(trace.getAttributes());
+			for(XEvent newEvent : newTrace)
+				trace.add(newEvent);
+		}
+		
+		PluginContext pluginContext = RapidProMGlobalContext.instance().getPluginContext();
+
+		XLogIOObject result = new XLogIOObject(clone, pluginContext);
+		
+		outputEventLog.deliver(result);
+
+		logger.log(Level.INFO, "End: Only contain lifecycle complete events ("
+				+ (System.currentTimeMillis() - time) / 1000 + " sec)");
+	}
+
+}
---
+++ src/main/java/org/rapidprom/operators/niek/RandomLogFilter.java
@@ -0,0 +1,99 @@
+package org.rapidprom.operators.niek;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import org.deckfour.xes.classification.XEventClass;
+import org.deckfour.xes.classification.XEventClasses;
+import org.deckfour.xes.classification.XEventNameClassifier;
+import org.deckfour.xes.info.XLogInfo;
+import org.deckfour.xes.info.XLogInfoFactory;
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
+import org.deckfour.xes.model.impl.XAttributeLiteralImpl;
+import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginVariant;
+
+
+@Plugin(
+		name = "Filter Log using Randomness", 
+		parameterLabels = {"Input Log"}, 
+	    returnLabels = {"Logs"}, 
+	    returnTypes = { List.class }
+		)
+public class RandomLogFilter{
+	
+	public static XLog projectLogOnEventNames(final XLog log, final Set<String> eventSet){
+		XLog logClone = (XLog) log.clone();
+		Set<XTrace> traceRemoveSet = new HashSet<XTrace>();
+		for(XTrace trace : logClone){
+			Set<XEvent> eventRemoveSet = new HashSet<XEvent>();
+			for(XEvent event : trace){
+				if(!eventSet.contains(event.getAttributes().get("concept:name").toString()))
+					eventRemoveSet.add(event);						
+			}
+			for(XEvent removeEvent : eventRemoveSet)
+				trace.remove(removeEvent);
+			if(trace.size()==0)
+				traceRemoveSet.add(trace);
+		}
+		for(XTrace removeTrace : traceRemoveSet)
+			logClone.remove(removeTrace);
+		return logClone;
+	}
+	
+	@UITopiaVariant(affiliation = UITopiaVariant.EHV, author = "N. Tax", email = "n.tax@tue.nl")
+	@PluginVariant(variantLabel = "Filter Log using Randomness", requiredParameterLabels = {0})
+	public List<XLog> getProjections(PluginContext context, XLog log) {
+		List<XLog> logs = new ArrayList<XLog>();
+
+		// build initial set
+		XLogInfo info = XLogInfoFactory.createLogInfo(log, new XEventNameClassifier());
+		XEventClasses activities = info.getEventClasses();
+		List<XEventClass> sortedActivities = sortEvents(activities);
+		Set<XEventClass> currentActivities = new HashSet<XEventClass>(sortedActivities);
+		int size = activities.size();
+		for(XEventClass activityToRemove : sortedActivities){
+			currentActivities.remove(activityToRemove);
+			size--;
+			XLog tempLog = projectLogOnEventNames(log, getNames(currentActivities));
+			tempLog.getAttributes().put("concept:name", new XAttributeLiteralImpl("concept:name", "size "+size));
+			logs.add(tempLog);
+		}
+		
+		Collections.reverse(logs);
+
+		return logs;
+	}
+	
+	private Set<String> getNames(Collection<XEventClass> activities){
+		Set<String> names = new HashSet<String>();
+		for(XEventClass activity : activities)
+			names.add(activity.getId());
+		return names;
+	}
+	
+	/**
+	 * Sorts the given event classes randomly.
+	 * 
+	 * @param eventClasses
+	 *            The given event classes.
+	 * @return The sorted event classes.
+	 */
+	private ArrayList<XEventClass> sortEvents(XEventClasses eventClasses) {
+		long seed = System.nanoTime();
+		ArrayList<XEventClass> sortedEvents = new ArrayList<XEventClass>();
+		for (XEventClass event : eventClasses.getClasses())
+			sortedEvents.add(event);
+		Collections.shuffle(sortedEvents, new Random(seed));
+		return sortedEvents;
+	}
+}
