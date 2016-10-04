5acd4c6ee33ff243f17ddb794cc942e6ee4bcdcd
Eduardo Gonzalez <edu.gonza.lopez@gmail.com>
2016-10-04 12:07:34 +0200

    Added file with constants for padas operators

---
+++ src/main/java/org/rapidprom/operators/padas/PADASConstants.java
@@ -0,0 +1,38 @@
+package org.rapidprom.operators.padas;
+
+public class PADASConstants {
+
+	public static final String AT_DATAMODEL = "datamodel";
+	public static final String AT_CLASS = "class";
+	public static final String AT_ATNAME = "attribute_name";
+	public static final String AT_RS_NAME = "relationship_name";
+	public static final String AT_RS_CLASS_SOURCE = "class_source";
+	public static final String AT_RS_CLASS_TARGET = "class_target";
+	public static final String AT_RS_ATNAME_SOURCE = "attribute_source";
+	public static final String AT_RS_ATNAME_TARGET = "attribute_target";
+	
+	public final static String EV_EXSET_ANNOTATION_FILE_NAME = "file_name";
+	public final static String EV_EXSET_ANNOTATION_TIMESTAMP = "timestamp";
+	public final static String EV_EXSET_ANNOTATION_TIMESTAMP_FORMAT = "timestamp_format";
+	public final static String EV_EXSET_ANNOTATION_ACTIVITY = "activity";
+	public final static String EV_EXSET_ANNOTATION_ACTIVITY_INSTANCE = "activity_instance";
+	public final static String EV_EXSET_ANNOTATION_LIFECYCLE = "lifecycle";
+	public final static String EV_EXSET_ANNOTATION_RESOURCE = "resource";
+	
+	public final static String VER_EXSET_ANNOTATION_FILE_NAME = "file_name";
+	public final static String VER_EXSET_ANNOTATION_DATAMODEL_NAME = "datamodel_name";
+	public final static String VER_EXSET_ANNOTATION_CLASS_NAME = "class_name";
+	public final static String VER_EXSET_ANNOTATION_START_TIMESTAMP = "start_timestamp";
+	public final static String VER_EXSET_ANNOTATION_END_TIMESTAMP = "end_timestamp";
+	public final static String VER_EXSET_ANNOTATION_TIMESTAMP_FORMAT = "timestamp_format";
+	
+	public final static String EV_TO_VER_MAPPING_EVID_FIELD = "eventId";
+	public final static String EV_TO_VER_MAPPING_VERID_FIELD = "versionId";
+	public final static String EV_TO_VER_MAPPING_LABEL_FIELD = "label";
+	
+	public final static String LOGS_PROC_ID_FIELD = "processId";
+	public final static String LOGS_LOG_ID_FIELD = "logId";
+	public final static String LOGS_TRACE_ID_FIELD = "traceId";
+	public final static String LOGS_EVENT_ID_FIELD = "eventId";
+	
+}
