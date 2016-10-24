6188216406923aa4cf376cda2444a10a2f66f248
abolt <a.bolt@tue.nl>
2016-10-24 19:42:10 +0200

    new reduction operator (work in progress)

---
+++ src/main/java/org/rapidprom/operators/analysis/MurataReductionOperator.java
@@ -0,0 +1,13 @@
+package org.rapidprom.operators.analysis;
+
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+
+public class MurataReductionOperator extends Operator {
+
+	public MurataReductionOperator(OperatorDescription description) {
+		super(description);
+		
+	}
+
+}
