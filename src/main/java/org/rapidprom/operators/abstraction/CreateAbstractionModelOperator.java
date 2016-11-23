8e000e43ca487095582f2ca1df4732b1f06f92e4
Felix Mannhardt <f.mannhardt@tue.nl>
2016-11-23 08:26:09 +0100

    Default composition parameter added

--- src/main/java/org/rapidprom/operators/abstraction/CreateAbstractionModelOperator.java
+++ src/main/java/org/rapidprom/operators/abstraction/CreateAbstractionModelOperator.java
@@ -3,8 +3,11 @@ package org.rapidprom.operators.abstraction;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import javax.lang.model.SourceVersion;
 
 import org.processmining.datapetrinets.DataPetriNet;
 import org.processmining.framework.plugin.PluginContext;
@@ -31,6 +34,7 @@ import com.rapidminer.operator.ports.OutputPort;
 import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
 import com.rapidminer.operator.ports.metadata.MetaData;
 import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeCategory;
 import com.rapidminer.parameter.ParameterTypeString;
 import com.rapidminer.parameter.UndefinedParameterError;
 
@@ -41,6 +45,9 @@ import com.rapidminer.parameter.UndefinedParameterError;
  *
  */
 public class CreateAbstractionModelOperator extends Operator {
+
+	private static final String INTERLEAVING = "Interleaving (%)";
+	private static final String PARALLEL = "Parallel (#)";
 
 	private final static class AbstractionCodeResult extends ResultObjectAdapter {
 		private static final long serialVersionUID = 5985457282363620702L;
@@ -57,15 +64,19 @@ public class CreateAbstractionModelOperator extends Operator {
 		}
 	}
 
-	private static final String ABSTRACTION_MODEL_KEY = "Abstraction Model",
-			ABSTRACTION_MODEL_DESCR = "<HTML>Specify how patterns are composed to an integrated abstraction model. "
-					+ "If left empty all patterns can occur in parallel to each other, i.e, for patterns A,B,C, the abstraction model is A*|B*|C*."
+	private static final String DEFAULT_COMPOSITION_KEY = "Default Composition",
+			DEFAULT_COMPOSITION_DESCR = "Speciy how activity patterns are composed by default.",
+			ABSTRACTION_MODEL_KEY = "Abstraction Model",
+			ABSTRACTION_MODEL_DESCR = "<HTML>Specify how activity patterns are composed to an integrated abstraction model. "
+					+ "If left empty all patterns are composed with the default composition (parallel or interleaving), e.g, for patterns A,B,C, the abstraction model is A*#B*#C*."
 					+ "<h1>Model Syntax</h1>" + "Initially all abstraction patterns are composed in parallel. "
 					+ "The composition can be changed by manipulating the formula or by using the context menu (right-click) of the model."
 					+ "Operators can be nested, patterns can appear multiple times and parentheses can be used to indicate preceedence if necessary."
 					+ "<h2>Examples for patterns A and B</h2>" + "<ul>"
-					+ "<li>'A#B' - A and B can happen in parallel</li>" + "<li>'A|B' - Either A or B can happen</li>"
-					+ "<li>'A<B' - A must happen before B</li>" + "<li>'%[A,B]' - A and B can interleave</li>"
+					+ "<li>'A#B' - A and B can happen in parallel</li>" 
+					+ "<li>'A|B' - Either A or B can happen</li>"
+					+ "<li>'A<B' - A must happen before B</li>" 
+					+ "<li>'%[A,B]' - A and B can interleave</li>"
 					+ "<li>'A?' - A can happen 1 or 0 times</li>" + "<li>'A+' - A can happen 1 or multiple times</li>"
 					+ "<li>'A*' - A can happen 0 or multiple times</li>"
 					+ "<li>'A{n,m}' - A can happen n to m times</li>" + "</ul>" + "<h2>Full Syntax</h2>" + "<pre>"
@@ -129,8 +140,35 @@ public class CreateAbstractionModelOperator extends Operator {
 		String modelAsString = getParameterAsString(ABSTRACTION_MODEL_KEY);
 		if (modelAsString != null && !modelAsString.isEmpty()) {
 			return modelAsString;
+		} else {
+			String defaultComposition = getParameter(DEFAULT_COMPOSITION_KEY);
+			if (PARALLEL.equals(defaultComposition)) {
+				return PatternBasedLogAbstractionPlugin.buildInitialModel(abstractionPatterns);				
+			} else if (INTERLEAVING.equals(defaultComposition)) {
+				return buildInitialModelInInterleaving(abstractionPatterns);
+			} else {
+				throw new IllegalArgumentException("Unknown default composition "+ defaultComposition);
+			}
 		}
-		return PatternBasedLogAbstractionPlugin.buildInitialModel(abstractionPatterns);
+	}
+	
+	//TODO update in LogEnhancement
+	private static String buildInitialModelInInterleaving(Map<String, AbstractionPattern> abstractionPatterns) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("%[");
+		for (Iterator<String> iter = abstractionPatterns.keySet().iterator(); iter.hasNext();) {
+			String id = iter.next();
+			if (!SourceVersion.isIdentifier(id)) {
+				id = "\"" + id + "\"";
+			}
+			sb.append(id);
+			sb.append("*");
+			if (iter.hasNext()) {
+				sb.append(",");
+			}
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 
 	private DataPetriNet[] getPatterns() throws UserError, OperatorException {
@@ -153,6 +191,8 @@ public class CreateAbstractionModelOperator extends Operator {
 	public List<ParameterType> getParameterTypes() {
 		List<ParameterType> params = super.getParameterTypes();
 		params.add(new ParameterTypeString(ABSTRACTION_MODEL_KEY, ABSTRACTION_MODEL_DESCR, true, false));
+		params.add(new ParameterTypeCategory(DEFAULT_COMPOSITION_KEY, DEFAULT_COMPOSITION_DESCR,
+				new String[] { PARALLEL, INTERLEAVING }, 0));
 		return params;
 	}
 
