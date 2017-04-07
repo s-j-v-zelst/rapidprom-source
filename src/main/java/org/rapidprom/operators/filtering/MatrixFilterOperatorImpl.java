0c3e015000cb8e811c517986a67ad0dd74430124
fanisanim <m.fani.sani@tue.nl>
2017-04-07 14:35:15 +0200

    first version of Matrix Filter

--- src/main/java/org/rapidprom/operators/filtering/MatrixFilterOperatorImpl.java
+++ src/main/java/org/rapidprom/operators/filtering/MatrixFilterOperatorImpl.java
@@ -3,35 +3,37 @@ package org.rapidprom.operators.filtering;
 import java.util.List;
 
 import org.deckfour.xes.model.XLog;
-import org.processmining.noisefiltering.plugins.RProMNoiseFilterPlugin;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.logfiltering.parameters.MatrixFilterParameter;
+import org.processmining.logfiltering.plugins.MatrixFilterPlugin;
 import org.rapidprom.external.connectors.prom.RapidProMGlobalContext;
 import org.rapidprom.ioobjects.XLogIOObject;
 import org.rapidprom.operators.filtering.abstr.AbstractFilteringOperator;
 
-import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
-import com.rapidminer.operator.ports.InputPort;
-import com.rapidminer.operator.ports.OutputPort;
-import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
 import com.rapidminer.parameter.ParameterType;
 import com.rapidminer.parameter.ParameterTypeDouble;
 
-public class FilterOutInfrequentBehaviorLogAutomatonOperatorImpl extends AbstractFilteringOperator {
+public class MatrixFilterOperatorImpl extends AbstractFilteringOperator {
 
 	private static final String PARAMETER_KEY_FILTER_THRESHOLD = "Threshold";
 	private static final String PARAMETER_DESC_FILTER_THRESHOLD = "Set the filtering threshold which is used in pruning the Anomaly-Free Automaton";
 
-	public FilterOutInfrequentBehaviorLogAutomatonOperatorImpl(OperatorDescription description) {
+	public MatrixFilterOperatorImpl(OperatorDescription description) {
 		super(description);
 	}
 
 	@Override
 	public void doWork() throws OperatorException {
 		XLog noisyLog = getInputLogPort().getData(XLogIOObject.class).getArtifact();
-		getOutputLogPort().deliver(new XLogIOObject(
-				RProMNoiseFilterPlugin.apply(noisyLog, getParameterAsDouble(PARAMETER_KEY_FILTER_THRESHOLD)),
-				RapidProMGlobalContext.instance().getPluginContext()));
+
+		MatrixFilterParameter parameters = new MatrixFilterParameter(
+				getXEventClassifier().getDefiningAttributeKeys()[0]);
+		parameters.setProbabilityOfRemoval(getParameterAsDouble(PARAMETER_KEY_FILTER_THRESHOLD));
+
+		PluginContext context = RapidProMGlobalContext.instance().getPluginContext();
+		getOutputLogPort().deliver(new XLogIOObject(MatrixFilterPlugin.run(context, noisyLog, parameters), context));
 	}
 
 	@Override
