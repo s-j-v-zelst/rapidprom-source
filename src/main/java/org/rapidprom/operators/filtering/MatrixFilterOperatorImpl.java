56e56b5d8958f8a67593b2b2604d94fe54fde575
fanisanim <m.fani.sani@tue.nl>
2017-08-09 15:26:14 +0200

    Remove/Select Also checking the order of options

--- src/main/java/org/rapidprom/operators/filtering/MatrixFilterOperatorImpl.java
+++ src/main/java/org/rapidprom/operators/filtering/MatrixFilterOperatorImpl.java
@@ -10,6 +10,7 @@ import org.processmining.framework.plugin.PluginContext;
 import org.processmining.hybridilpminer.parameters.LPFilterType;
 import org.processmining.logfiltering.parameters.AdjustingType;
 import org.processmining.logfiltering.parameters.FilterLevel;
+import org.processmining.logfiltering.parameters.FilterSelection;
 import org.processmining.logfiltering.parameters.MatrixFilterParameter;
 import org.processmining.logfiltering.parameters.ProbabilityType;
 import org.processmining.logfiltering.plugins.MatrixFilterPlugin;
@@ -39,7 +40,7 @@ public class MatrixFilterOperatorImpl extends AbstractFilteringOperator {
 	
 	private static final String PARAMETER_DESC_Filtering_type = "Set the method of Filtering Event Base or Trace Base!";
 	private static final String PARAMETER_KEY_Filtering_Type = "Event/Trace";
-	private static final String[] Filtering_Type_OPTIONS = new String[] { FilterLevel.EVENT.toString(), FilterLevel.TRACE.toString()};
+	private static final String[] Filtering_Type_OPTIONS = new String[] {  FilterLevel.TRACE.toString(),FilterLevel.EVENT.toString()};
 	private static final FilterLevel[] Filtering_Type_OPTIONS_List= new FilterLevel[]{ FilterLevel.TRACE,FilterLevel.EVENT};
 	
 	private static final String PARAMETER_KEY_Probability_Method = "Measuring Probabilities";
@@ -47,6 +48,10 @@ public class MatrixFilterOperatorImpl extends AbstractFilteringOperator {
 	private static final String[] Probability_OPTIONS = new String[] { ProbabilityType.DIRECT.toString(), ProbabilityType.REVERSE.toString(),ProbabilityType.MIX.toString(),ProbabilityType.AFA.toString() };
 	private static final ProbabilityType[] Probability_OPTIONS_List= new ProbabilityType[]{ ProbabilityType.DIRECT,ProbabilityType.REVERSE, ProbabilityType.MIX,ProbabilityType.AFA};
 	
+	private static final String PARAMETER_KEY_Selection_Method = "Outlier Selection";
+	private static final String PARAMETER_DESC_Selection_Method = "Do you want to Remove or Keeping the Outlires";
+	private static final String[] Selection_OPTIONS = new String[] { FilterSelection.REMOVE.toString() ,FilterSelection.SELECT.toString() };
+	private static final FilterSelection[] Selection_OPTIONS_List= new FilterSelection[]{ FilterSelection.REMOVE ,FilterSelection.SELECT};
 	public MatrixFilterOperatorImpl(OperatorDescription description) {
 		super(description);
 	}
@@ -63,6 +68,7 @@ public class MatrixFilterOperatorImpl extends AbstractFilteringOperator {
 		parameters.setSubsequenceLength(getParameterAsInt(PARAMETER_KEY_SUBSEQUENT_LENGTH));
 		parameters.setFilterLevel(Filtering_Type_OPTIONS_List[getParameterAsInt(PARAMETER_KEY_Filtering_Type)]);
 		parameters.setProbabilitycomutingMethod(Probability_OPTIONS_List[getParameterAsInt(PARAMETER_KEY_Probability_Method)]);
+		parameters.setFilteringSelection(Selection_OPTIONS_List[getParameterAsInt(PARAMETER_KEY_Selection_Method)]);
 		PluginContext context = RapidProMGlobalContext.instance().getPluginContext();
 		getOutputLogPort().deliver(new XLogIOObject(MatrixFilterPlugin.run(context, noisyLog, parameters), context));
 	}
@@ -79,6 +85,8 @@ public class MatrixFilterOperatorImpl extends AbstractFilteringOperator {
 		params.add(new ParameterTypeCategory(PARAMETER_KEY_Filtering_Type, PARAMETER_DESC_Filtering_type, Filtering_Type_OPTIONS, 0,
 				false));
 		params.add(new ParameterTypeCategory(PARAMETER_KEY_Probability_Method, PARAMETER_DESC_Probability_Method, Probability_OPTIONS, 0,
+				false));
+		params.add(new ParameterTypeCategory(PARAMETER_KEY_Selection_Method, PARAMETER_DESC_Selection_Method, Selection_OPTIONS, 0,
 				false));
 		return params;
 	}
