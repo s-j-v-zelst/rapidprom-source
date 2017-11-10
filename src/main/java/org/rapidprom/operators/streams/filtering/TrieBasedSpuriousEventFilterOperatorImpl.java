07bebef3f7159c0796f4de3a08dc7d9b8ae201fa
svzelst <s.j.v.zelst@gmail.com>
2017-11-10 17:51:43 +0100

    update references to libraries encorporate newly added parameters to the filter.

--- src/main/java/org/rapidprom/operators/streams/filtering/TrieBasedSpuriousEventFilterOperatorImpl.java
+++ src/main/java/org/rapidprom/operators/streams/filtering/TrieBasedSpuriousEventFilterOperatorImpl.java
@@ -1,5 +1,6 @@
 package org.rapidprom.operators.streams.filtering;
 
+import java.util.EnumSet;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,6 +12,9 @@ import org.processmining.stream.core.enums.CommunicationType;
 import org.processmining.stream.core.interfaces.XSAuthor;
 import org.processmining.streambasedeventfilter.algorithms.ConditionalProbabilitiesBasedXSEventFilterImpl;
 import org.processmining.streambasedeventfilter.parameters.ConditionalProbabilitiesBasedXSEventFilterParametersImpl;
+import org.processmining.streambasedeventfilter.parameters.ConditionalProbabilitiesBasedXSEventFilterParametersImpl.Abstraction;
+import org.processmining.streambasedeventfilter.parameters.ConditionalProbabilitiesBasedXSEventFilterParametersImpl.AdjustmentMethod;
+import org.processmining.streambasedeventfilter.parameters.ConditionalProbabilitiesBasedXSEventFilterParametersImpl.FilteringMethod;
 import org.processmining.streambasedeventlog.parameters.StreamBasedEventLogParametersImpl;
 import org.processmining.yawl.ext.org.apache.commons.lang.ArrayUtils;
 import org.rapidprom.external.connectors.prom.RapidProMGlobalContext;
@@ -18,7 +22,9 @@ import org.rapidprom.ioobjects.streams.XSAuthorIOObject;
 import org.rapidprom.ioobjects.streams.XSHubIOObject;
 import org.rapidprom.ioobjects.streams.event.XSEventStreamIOObject;
 import org.rapidprom.operators.streams.generators.XLogToEventStreamOperatorImpl;
+import org.rapidprom.util.ObjectUtils;
 
+import com.kenai.jffi.Array;
 import com.rapidminer.example.Attribute;
 import com.rapidminer.example.ExampleSet;
 import com.rapidminer.example.table.AttributeFactory;
@@ -37,6 +43,7 @@ import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
 import com.rapidminer.operator.ports.metadata.MDInteger;
 import com.rapidminer.parameter.ParameterType;
 import com.rapidminer.parameter.ParameterTypeBoolean;
+import com.rapidminer.parameter.ParameterTypeCategory;
 import com.rapidminer.parameter.ParameterTypeDouble;
 import com.rapidminer.parameter.ParameterTypeInt;
 import com.rapidminer.parameter.ParameterTypeString;
@@ -72,12 +79,8 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 	private final static double PARAM_DEFAULT_INCLUSION_THRESHOLD = 0.2;
 
 	private final static String PARAM_KEY_MAX_LOOKAHEAD = "Maximum Look-ahead";
-	private final static String PARAM_DESC_MAX_LOOKAHEAD = "In case we decide to label an event as being spurious, we are allowed to look-ahead in the trie to decide whether or not the label relates to parallel behaviour.";
+	private final static String PARAM_DESC_MAX_LOOKAHEAD = "In case we decide to label an event as being spurious, we are allowed to look-ahead / back in the trace to decide whether or not the label relates to parallel behaviour.";
 	private final static byte PARAM_DEFAULT_MAX_LOOKAHEAD = 3;
-
-	private final static String PARAM_KEY_LOOKAHEAD_INCLUSION_THRESHOLD = "Look-ahead Inclusion Threshold";
-	private final static String PARAM_DESC_LOOKAHEAD_INCLUSION_THRESHOLD = "Threshold to be applied within the lookahead filter";
-	private final static double PARAM_DEFAULT_LOOKAHEAD_INCLUSION_THRESHOLD = 0.2;
 
 	private final static String PARAM_KEY_NOISE_LABEL_KEY = "Noise Label Key";
 	private final static String PARAM_DESC_NOISE_LABEL_KEY = "In case of an experiment, this indicates the data packet key which allows us to classify the event as either being noise or not. Note that the stream generator always prepends xsevent:data: to the actual key within the input event log/stream";
@@ -86,6 +89,21 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 	private final static String PARAM_KEY_NOISE_LABEL_VALUE = "Noise Label Value";
 	private final static String PARAM_DESC_NOISE_LABEL_VALUE = "In case of an experiment, this indicates the data packet attribute value which allows us to classify the event as either being noise or not";
 	private final static String PARAM_DEFAULT_NOISE_LABEL_VALUE = "true";
+
+	private final static String PARAM_KEY_FILTER_TECHNIQUE = "Filter Technique";
+	private final static String PARAM_DESC_FILTER_TEHCNIQUE = "Allows us to determine what filter strategy to use, i.e. what window to incorporate.";
+	private final static FilteringMethod[] PARAM_VALUES_FILTER_TECHNIQUE = EnumSet.allOf(FilteringMethod.class)
+			.toArray(new FilteringMethod[EnumSet.allOf(FilteringMethod.class).size()]);
+
+	private final static String PARAM_KEY_ADJUSTMENT_METHOD = "Adjustment Method";
+	private final static String PARAM_DESC_ADJUSTMENT_METHOD = "Allows us to determine how to internally filter.";
+	private final static AdjustmentMethod[] PARAM_VALUES_ADJUSTMENT_METHOD = EnumSet.allOf(AdjustmentMethod.class)
+			.toArray(new AdjustmentMethod[EnumSet.allOf(AdjustmentMethod.class).size()]);
+
+	private final static String PARAM_KEY_ABSTRACTION = "Abstraction";
+	private final static String PARAM_DESC_ABSTRACTION = "What abstraction to use to define the current state in the Markov chain / conditional part of probability.";
+	private final static Abstraction[] PARAM_VALUES_ABSTRACTION = EnumSet.allOf(Abstraction.class)
+			.toArray(new Abstraction[EnumSet.allOf(Abstraction.class).size()]);
 
 	private final short TIME_OUT = 1000;
 
@@ -101,13 +119,17 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 	private final String COLUMN_F1_SCORE = "f1_score";
 
 	private final String[] QUALITY_METRICS_COLUMN_NAMES = new String[] { COLUMN_TRUE_POSITIVE, COLUMN_FALSE_POSITIVE,
-			COLUMN_TRUE_NEGATIVE, COLUMN_FALSE_NEGATIVE };
+			COLUMN_TRUE_NEGATIVE, COLUMN_FALSE_NEGATIVE, COLUMN_RECALL, COLUMN_PRECISION, COLUMN_SPECIFITY, COLUMN_NPV,
+			COLUMN_ACCURACY, COLUMN_F1_SCORE };
 
 	private final int[] QUALITY_METRICS_COLUMN_TYPES = new int[] { Ontology.INTEGER, Ontology.INTEGER, Ontology.INTEGER,
-			Ontology.INTEGER };
+			Ontology.INTEGER, Ontology.REAL, Ontology.REAL, Ontology.REAL, Ontology.REAL, Ontology.REAL,
+			Ontology.REAL };
 
 	private final String[] QUALITY_METRICS_COLUMN_ROLES = new String[] { AttributeColumn.REGULAR,
-			AttributeColumn.REGULAR, AttributeColumn.REGULAR, AttributeColumn.REGULAR };
+			AttributeColumn.REGULAR, AttributeColumn.REGULAR, AttributeColumn.REGULAR, AttributeColumn.REGULAR,
+			AttributeColumn.REGULAR, AttributeColumn.REGULAR, AttributeColumn.REGULAR, AttributeColumn.REGULAR,
+			AttributeColumn.REGULAR };
 
 	public TrieBasedSpuriousEventFilterOperatorImpl(OperatorDescription description) {
 		super(description);
@@ -131,22 +153,16 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 		logger.log(Level.INFO, "start do work filter spurious events");
 		StreamBasedEventLogParametersImpl storageParams = new StreamBasedEventLogParametersImpl();
 		storageParams.setSlidingWindowSize(getParameterAsInt(PARAM_KEY_WINDOW_SIZE));
-		// TrieBasedSpurioiusEventFilterParametersImpl filterParams = new
-		// TrieBasedSpurioiusEventFilterParametersImpl();
-		// filterParams.setInclusionDecisionThreshold(getParameterAsDouble(PARAM_KEY_INCLUSION_THRESHOLD));
-		// filterParams.setMaxLookAhead((byte)
-		// getParameterAsInt(PARAM_KEY_MAX_LOOKAHEAD));
-		// filterParams.setLookaheadInclusionDecisionThreshold(getParameterAsDouble(PARAM_KEY_LOOKAHEAD_INCLUSION_THRESHOLD));
-		// filterParams.setEmissionDelay(getParameterAsInt(PARAM_KEY_EMISSION_DELAY));
 		ConditionalProbabilitiesBasedXSEventFilterParametersImpl filterParams = new ConditionalProbabilitiesBasedXSEventFilterParametersImpl();
+		filterParams.setAbstraction(PARAM_VALUES_ABSTRACTION[getParameterAsInt(PARAM_KEY_ABSTRACTION)]);
+		filterParams.setFiltermethod(PARAM_VALUES_FILTER_TECHNIQUE[getParameterAsInt(PARAM_KEY_FILTER_TECHNIQUE)]);
+		filterParams
+				.setAdjustmentmethod(PARAM_VALUES_ADJUSTMENT_METHOD[getParameterAsInt(PARAM_KEY_ADJUSTMENT_METHOD)]);
 		filterParams.setMaxPatternLength(getParameterAsInt(PARAM_KEY_MAX_LOOKAHEAD));
 		filterParams.setCutoffThreshold(getParameterAsDouble(PARAM_KEY_INCLUSION_THRESHOLD));
 		filterParams.setExperiment(getParameterAsBoolean(PARAM_KEY_IS_EXPERIMENT));
 		filterParams.setNoiseClassificationLabelKey(getParameterAsString(PARAM_KEY_NOISE_LABEL_KEY));
 		filterParams.setNoiseClassificationLabelValue(getParameterAsString(PARAM_KEY_NOISE_LABEL_VALUE));
-
-		// TrieBasedSpuriousEventFilterImpl hub = new
-		// TrieBasedSpuriousEventFilterImpl(storageParams, filterParams);
 		ConditionalProbabilitiesBasedXSEventFilterImpl hub = new ConditionalProbabilitiesBasedXSEventFilterImpl(
 				filterParams, storageParams);
 		XSEventStream out = XSEventStreamFactory.createXSEventStream(CommunicationType.SYNC);
@@ -182,6 +198,24 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_FALSE_POSITIVE)] = hub.getFalsePositives();
 			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_TRUE_NEGATIVE)] = hub.getTrueNegatives();
 			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_FALSE_NEGATIVE)] = hub.getFalseNegatives();
+			final double recall = ((double) hub.getTruePositives())
+					/ ((double) (hub.getTruePositives() + hub.getFalseNegatives()));
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_RECALL)] = recall;
+			final double precision = ((double) hub.getTruePositives())
+					/ ((double) (hub.getTruePositives() + hub.getFalsePositives()));
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_PRECISION)] = precision;
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES,
+					COLUMN_SPECIFITY)] = ((double) hub.getTrueNegatives())
+							/ ((double) (hub.getTrueNegatives() + hub.getFalsePositives()));
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_NPV)] = ((double) hub.getTrueNegatives())
+					/ ((double) (hub.getTrueNegatives() + hub.getFalseNegatives()));
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES,
+					COLUMN_ACCURACY)] = ((double) hub.getTruePositives() + hub.getTrueNegatives())
+							/ ((double) (hub.getTruePositives() + hub.getTrueNegatives() + hub.getFalsePositives()
+									+ hub.getFalseNegatives()));
+			values[ArrayUtils.indexOf(QUALITY_METRICS_COLUMN_NAMES, COLUMN_F1_SCORE)] = (2 * recall * precision)
+					/ (recall + precision);
+			
 			builder.addDataRow(dataRowFactory.create(values, attributes));
 			outputExperimentResult.deliver(builder.build());
 			// shutdown that shit
@@ -204,6 +238,18 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 				Integer.MAX_VALUE, PARAM_DEFAULT_WINDOW_SIZE, false);
 		params.add(slidingWindowSize);
 
+		ParameterTypeCategory abstraction = new ParameterTypeCategory(PARAM_KEY_ABSTRACTION, PARAM_DESC_ABSTRACTION,
+				ObjectUtils.toString(PARAM_VALUES_ABSTRACTION), 0, false);
+		params.add(abstraction);
+
+		ParameterTypeCategory filteringMethod = new ParameterTypeCategory(PARAM_KEY_FILTER_TECHNIQUE,
+				PARAM_DESC_FILTER_TEHCNIQUE, ObjectUtils.toString(PARAM_VALUES_FILTER_TECHNIQUE), 0, false);
+		params.add(filteringMethod);
+
+		ParameterTypeCategory adjustment = new ParameterTypeCategory(PARAM_KEY_ADJUSTMENT_METHOD,
+				PARAM_DESC_ADJUSTMENT_METHOD, ObjectUtils.toString(PARAM_VALUES_ADJUSTMENT_METHOD), 0, false);
+		params.add(adjustment);
+
 		ParameterTypeDouble inclusionThreshold = new ParameterTypeDouble(PARAM_KEY_INCLUSION_THRESHOLD,
 				PARAM_DESC_INCLUSION_THRESHOLD, 0, 1, PARAM_DEFAULT_INCLUSION_THRESHOLD, false);
 		params.add(inclusionThreshold);
@@ -211,11 +257,6 @@ public class TrieBasedSpuriousEventFilterOperatorImpl extends Operator {
 		ParameterTypeInt maxLookAhead = new ParameterTypeInt(PARAM_KEY_MAX_LOOKAHEAD, PARAM_DESC_MAX_LOOKAHEAD, 0,
 				Byte.MAX_VALUE, PARAM_DEFAULT_MAX_LOOKAHEAD, false);
 		params.add(maxLookAhead);
-
-		ParameterTypeDouble lookAheadInclusionThreshold = new ParameterTypeDouble(
-				PARAM_KEY_LOOKAHEAD_INCLUSION_THRESHOLD, PARAM_DESC_LOOKAHEAD_INCLUSION_THRESHOLD, 0, 1,
-				PARAM_DEFAULT_LOOKAHEAD_INCLUSION_THRESHOLD, false);
-		params.add(lookAheadInclusionThreshold);
 
 		ParameterTypeInt emissionDelay = new ParameterTypeInt(PARAM_KEY_EMISSION_DELAY, PARAM_DESC_EMISSION_DELAY, 0,
 				Integer.MAX_VALUE, PARAM_DEFAULT_EMISSION_DELAY, true);
